325eb3b0b6c0ac97770a0283cc2785edbe141839
Mindy Preston <mindy.preston@docker.com>
2016-11-14 19:48:29 -0600

    printf will save us!

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,4 +3,5 @@ git clone -b targets https://github.com/yomimono/mirage-skeleton.git
 cd mirage-skeleton
 eval `opam config env`
 find . -name _build | xargs rm -rf
-export MODE && make
+echo "hello my mode is ${MODE}"
+make
