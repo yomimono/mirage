7ab5c90ad92c0f89ced4bf12f8e1d9e541a1e9eb
Mindy Preston <mindy.preston@docker.com>
2017-01-16 22:48:11 -0600

    try to build test unikernel before trying mirage-skeleotn

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
+cd test && mirage configure -t ${MODE} && make depend && make && mirage clean && cd ..
 git clone -b mirage-dev https://github.com/mirage/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton
