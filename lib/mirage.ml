637b167d2fb5c1564a609a52690f6b56314ccb91
Mindy <meetup@yomimono.org>
2018-07-27 18:27:45 -0500

    force some gcc flags (remove this)

--- lib/mirage.ml
+++ lib/mirage.ml
@@ -1865,7 +1865,7 @@ let compile libs warn_error target =
   and result = match target with
     | `Unix | `MacOSX -> "main.native"
     | `Xen | `Qubes | `Virtio | `Ukvm | `Muen -> "main.native.o"
-  and cflags = [ "-g" ]
+  and cflags = [ "-g"; "-mno-red-zone"; "-fno-stack-protector"]
   and lflags =
     let dontlink =
       match target with
